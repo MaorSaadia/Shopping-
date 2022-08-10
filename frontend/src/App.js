bc8458f139c65ecf1bbe1f48ee86d80289ec31cb
Maor <maorsa9@gmail.com>
2022-08-10 12:19:58 +0300

    Added Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Container} from 'react-bootstrap'
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = ()=> {
   return (
@@ -9,7 +10,7 @@ const App = ()=> {
     <Header/>
     <main className='py-3'>
       <Container>
-        <h1>Welcome To Shopping+</h1>
+        <HomeScreen></HomeScreen>
       </Container>
       
     </main>
