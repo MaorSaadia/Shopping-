8f2579440df6f210b8e31127617dc44bad8828fb
Maor <maorsa9@gmail.com>
2022-08-30 19:10:17 +0300

    Starting Working with the admin user

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,12 +1,12 @@
 const products = [
   {
-    name: 'Airpods Wireless Bluetooth Headphones',
+    name: 'Airpods Pro',
     image: '/images/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working.',
     brand: 'Apple',
     category: 'Electronics',
-    price: 89.99,
+    price: 129.99,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpadateProfileReducer,
+  userListReducer,
 } from './reducers/userReducer';
 import {
   orderCreateReducer,
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   userRegistor: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpadateProfile: userUpadateProfileReducer,
+  userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
@@ -63,55 +65,3 @@ const store = createStore(
 );
 
 export default store;
-
-// import { combineReducers, configureStore } from '@reduxjs/toolkit';
-// import {
-//   productListReducer,
-//   productDetailsReducer,
-// } from './reducers/productReducer';
-// import { cartReducer } from './reducers/cartReducer';
-// import {
-//   userLoginReducer,
-//   userRegisterReducer,
-//   userDetailsReducer,
-// } from './reducers/userReducer';
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const cartItemsFromStorage = localStorage.getItem('cartItems')
-//   ? JSON.parse(localStorage.getItem('cartItems'))
-//   : [];
-
-// const userInfoFromStorage = localStorage.getItem('userInfo')
-//   ? JSON.parse(localStorage.getItem('userInfo'))
-//   : null;
-
-// const cart = createSlice({
-//   name: 'cart',
-//   initialState: { cartItems: cartItemsFromStorage },
-//   reducers: {
-//     cart: cartReducer,
-//   },
-// });
-
-// const userLogin = createSlice({
-//   name: 'userLogin',
-//   initialState: { userInfo: userInfoFromStorage },
-//   reducers: {
-//     userLogin: userLoginReducer,
-//   },
-// });
-
-// const reducer = combineReducers({
-//   productList: productListReducer,
-//   productDeatils: productDetailsReducer,
-//   cart: cart.reducer,
-//   userLogin: userLogin.reducer,
-//   userRegistor: userRegisterReducer,
-//   userDetails: userDetailsReducer,
-// });
-
-// const store = configureStore({
-//   reducer,
-// });
-
-// export default store;
